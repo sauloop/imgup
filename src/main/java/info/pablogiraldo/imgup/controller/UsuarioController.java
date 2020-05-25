7ddb8337529d5c54b4c1312d30991c920a5488e9
Pablo Giraldo <pablogirald@yahoo.es>
2020-05-25 23:58:35 +0200

    Test

--- src/main/java/info/pablogiraldo/imgup/controller/UsuarioController.java
+++ src/main/java/info/pablogiraldo/imgup/controller/UsuarioController.java
@@ -57,7 +57,7 @@ public class UsuarioController {
 
 //			String ruta = "src/main/resources/static/img/";
 
-			String relativeWebPath = "/img/";
+			String relativeWebPath = "/";
 			String ruta = context.getRealPath(relativeWebPath);
 
 //			String ruta = "\\META-INF.resources\\img\\";
