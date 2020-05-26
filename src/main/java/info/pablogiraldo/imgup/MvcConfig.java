12ab8c4b88e942fefd87e0afbb33bbd388ef76e2
Pablo Giraldo <pablogirald@yahoo.es>
2020-05-26 10:37:12 +0200

    Test

--- src/main/java/info/pablogiraldo/imgup/MvcConfig.java
+++ src/main/java/info/pablogiraldo/imgup/MvcConfig.java
@@ -20,7 +20,7 @@ public class MvcConfig implements WebMvcConfigurer {
 
 //		registry.addResourceHandler("/img/**").addResourceLocations("file:/C:/pruebas/img/");
 
-		String ruta = context.getRealPath("/resources/uploads/");
+		String ruta = context.getRealPath("uploads");
 
 		registry.addResourceHandler("/uploads/**").addResourceLocations(ruta);
 
--- src/main/java/info/pablogiraldo/imgup/controller/UsuarioController.java
+++ src/main/java/info/pablogiraldo/imgup/controller/UsuarioController.java
@@ -57,7 +57,7 @@ public class UsuarioController {
 
 //			String ruta = "src/main/resources/static/img/";
 
-			String relativeWebPath = "/resources/uploads/";
+			String relativeWebPath = "uploads";
 			String ruta = context.getRealPath(relativeWebPath);
 
 //			String ruta = "\\META-INF.resources\\img\\";
