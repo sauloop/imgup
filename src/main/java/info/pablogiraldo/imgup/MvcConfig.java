1ac643251e2b2b4bb8267c3935dbc5a49d7f0c02
Pablo Giraldo <pablogirald@yahoo.es>
2020-05-26 11:03:28 +0200

    Test

--- src/main/java/info/pablogiraldo/imgup/MvcConfig.java
+++ src/main/java/info/pablogiraldo/imgup/MvcConfig.java
@@ -20,9 +20,9 @@ public class MvcConfig implements WebMvcConfigurer {
 
 //		registry.addResourceHandler("/img/**").addResourceLocations("file:/C:/pruebas/img/");
 
-		String ruta = context.getRealPath("/img/");
+//		String ruta = context.getRealPath("/img/");
 
-		registry.addResourceHandler("/img/**").addResourceLocations(ruta);
+		registry.addResourceHandler("/resources/**").addResourceLocations("/resources/");
 
 	}
 
